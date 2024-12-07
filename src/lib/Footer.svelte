ac980115b2b66851d0953ba3c9c78b1aa4610220
matheusmachado-dev <matheusmc.dev@gmail.com>
2024-12-06 21:56:55 -0300

    add `footer`

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<footer>
+	<div class="container">
+		<p>© 2024 Matheus Machado de Camargo. Todos os direitos reservados</p>
+	</div>
+</footer>
+
+<style>
+	footer {
+		font-size: 1rem;
+		width: 100%;
+		background-color: var(--cor-70);
+	}
+	.container {
+		width: 100%;
+		max-width: 1440px;
+		padding: 12px 24px;
+		margin: auto;
+	}
+</style>
